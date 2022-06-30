2f6e3fb143503dbd5e34913bddfbc04345137702
Ryan Mackie <mackie90125@gmail.com>
2022-06-30 17:35:00 -0500

    Version used for submission

--- src/titanic.R
+++ src/titanic.R
@@ -51,14 +51,16 @@ createTable(
 show.ratio = TRUE)
 
 # histograms for data distribution
-hist(d.train$Fare, breaks = 20)
-
+hist(d.train$Fare, breaks = 50)
+hist(d.train$Age)
+hist(d.train$SibSp)
+hist(d.train$Parch)
 
 ###############################################################################################
-### logistic regression ###
+### step logistic regression ###
 
 # data set to use for logistic regression
-d.logistic <- select(d.train, Survived, Pclass, Age, Sex, SibSp, Parch, Fare) %>%
+d.logistic <- select(d.train, Survived, Pclass, Sex, SibSp, Parch, Fare) %>%
 
   # remove any rows with missing values
   na.omit()
@@ -73,18 +75,144 @@ step(logistic.1, scope = Survived ~ 1, direction = "both", trace = FALSE)
 
 # chosen logistic regression model
 summary(
-  logistic.2 <- glm(formula = Survived ~ Pclass + Age + Sex + SibSp + Parch +
-    Fare + Pclass:Sex + Pclass:SibSp + Pclass:Parch + Age:Sex +
-    Age:Parch + Age:Fare, family = "binomial", data = d.logistic)
+  logistic.2 <- glm(formula = Survived ~ Pclass + Sex + SibSp + Parch + Fare +
+    Pclass:Sex + Pclass:SibSp + Pclass:Parch + Pclass:Fare +
+    Sex:Parch + Parch:Fare, family = "binomial", data = d.logistic)
 )
 
 # test if any elements should be removed
 drop1(logistic.2)
 
-# survival prediction for train data
-pred.train.logistic2 <- if_else(predict(logistic.2, newdata = d.logistic, type = "response") > 0.5, 1, 0)
+# function to return percent correct prediction from logistic regression model
+logistic.accuracy <- function(model, new_data, cutoff){
+  model.pred <- if_else(predict(model, newdata = new_data, type = "response") > cutoff, 1, 0)
+  return(sum(model.pred == new_data$Survived) / nrow(new_data) * 100)
+}
 
-# compare actual and predicted survival for train data
-table(pred.train.logistic2, d.logistic$Survived)
+# accuracy of step logistic model at different cutoff values
+cutoff.seq <- seq(0.05, 0.95, 0.05)
+cutoff.pred <- NULL
+for(i in 1:length(cutoff.seq)){
+  cutoff.pred[i] <- logistic.accuracy(logistic.2, d.train, cutoff.seq[i])
+}
+plot(cutoff.seq, cutoff.pred, ylim = c(0,100))
+max(cutoff.pred)
+cutoff.seq[which(cutoff.pred == max(cutoff.pred))]
 
-sum(pred.train.logistic2 == d.logistic$Survived) / nrow(d.logistic) * 100
+# original step model = 80.47138
+
+###############################################################################################
+### logistic regression with fare as a dichotomous variable ###
+
+summary(d.train$Fare)
+
+# try splitting Fare at median
+d.train$Fare_dich <- cut(d.train$Fare, c(-Inf, median(d.train$Fare), Inf))
+levels(d.train$Fare_dich) <- c("Low","High")
+
+# logistic regression with all possible two-way interactions
+logistic.3 <- glm(Survived ~ .^2,
+                  family = "binomial",
+                  data = select(d.train, Survived, Pclass, Sex, SibSp, Parch, Fare_dich))
+
+# step function to choose a logistic regression model
+step(logistic.3, scope = Survived ~ 1, direction = "both", trace = FALSE)
+
+summary(
+  logistic.4 <- glm(formula = Survived ~ Pclass + Sex + SibSp + Parch + Fare_dich +
+    Pclass:Sex + Pclass:SibSp + Pclass:Parch + Pclass:Fare_dich +
+    Sex:Parch + Parch:Fare_dich, family = "binomial", data = select(d.train,
+    Survived, Pclass, Sex, SibSp, Parch, Fare_dich))
+)
+
+logistic.accuracy(logistic.4, d.train, 0.5)
+
+# try different split values with same model
+split.seq <- c(1, seq(5,100,5))
+split.pred <- NULL
+
+for(i in 1:length(split.seq)){
+
+  # split Fare
+  d.train$Fare_dich <- cut(d.train$Fare, c(-Inf, split.seq[i], Inf))
+  levels(d.train$Fare_dich) <- c("Low","High")
+
+  # logistic regression with all possible two-way interactions
+  logistic.4 <- glm(formula = Survived ~ Pclass + Sex + SibSp + Parch + Fare_dich +
+    Pclass:Sex + Pclass:SibSp + Pclass:Parch + Pclass:Fare_dich +
+    Sex:Parch + Parch:Fare_dich, family = "binomial",
+    data = select(d.train, Survived, Pclass, Sex, SibSp, Parch, Fare_dich)
+  )
+
+  split.pred[i] <- logistic.accuracy(logistic.4, d.train, 0.5)
+}
+
+plot(split.seq, split.pred, ylim = c(0,100))
+max(split.pred)
+split.seq[which(split.pred == max(split.pred))]
+
+# model with Fare_dich = 81.14478
+
+# split fare at 20
+d.train$Fare_dich <- cut(d.train$Fare, c(-Inf, 20, Inf))
+levels(d.train$Fare_dich) <- c("Low","High")
+
+###############################################################################################
+### logistic regression with SibSp and Parch as a dichotomous variables ###
+
+# split both at 1
+d.train$SibSp_dich <- cut(d.train$SibSp, c(-Inf, 0.99, Inf))
+levels(d.train$SibSp_dich) <- c("None","Some")
+
+d.train$Parch_dich <- cut(d.train$Parch, c(-Inf, 0.99, Inf))
+levels(d.train$Parch_dich) <- c("None","Some")
+
+# logistic regression with all possible two-way interactions
+logistic.5 <- glm(Survived ~ .^2,
+                  family = "binomial",
+                  data = select(d.train, Survived, Pclass, Sex, SibSp_dich, Parch_dich, Fare_dich))
+
+# step function to choose a logistic regression model
+step(logistic.5, scope = Survived ~ 1, direction = "both", trace = FALSE)
+
+summary(
+  logistic.6 <- glm(formula = Survived ~ Pclass + Sex + Parch_dich + Fare_dich +
+    Pclass:Sex + Pclass:Parch_dich + Pclass:Fare_dich + Sex:Parch_dich +
+    Parch_dich:Fare_dich, family = "binomial", data = select(d.train,
+    Survived, Pclass, Sex, SibSp_dich, Parch_dich, Fare_dich))
+)
+
+logistic.accuracy(logistic.6, d.train, 0.5)
+
+# model with Fare_dich, Parch_dich and SibSp_dich = 81.25701
+
+# accuracy of logistic model at different cutoff values
+cutoff.seq <- seq(0.05, 0.95, 0.05)
+cutoff.pred <- NULL
+for(i in 1:length(cutoff.seq)){
+  cutoff.pred[i] <- logistic.accuracy(logistic.6, d.train, cutoff.seq[i])
+}
+plot(cutoff.seq, cutoff.pred, ylim = c(0,100))
+max(cutoff.pred)
+cutoff.seq[which(cutoff.pred == max(cutoff.pred))]
+
+###############################################################################################
+### predict survival from model 6 ###
+
+# create dichotomous variables in d.test data set
+d.test$Fare_dich <- cut(d.test$Fare, c(-Inf, 20, Inf))
+levels(d.test$Fare_dich) <- c("Low","High")
+
+d.test$SibSp_dich <- cut(d.test$SibSp, c(-Inf, 0.99, Inf))
+levels(d.test$SibSp_dich) <- c("None","Some")
+
+d.test$Parch_dich <- cut(d.test$Parch, c(-Inf, 0.99, Inf))
+levels(d.test$Parch_dich) <- c("None","Some")
+
+# d.test model predictions
+model.pred <- if_else(predict(logistic.6, newdata = d.test, type = "response") > 0.5, 1, 0)
+
+# create submission data frame
+d.sub <- data.frame(PassengerId = d.test$PassengerId,
+                    Survived = model.pred)
+write.csv(d.sub, "/Users/ryan/test_analysis/titanic/data/mackie_submission.csv", row.names = FALSE)
