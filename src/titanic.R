22198ac105991e20bcecfebb3c634c0dd96ae79a
Ryan Mackie <mackie90125@gmail.com>
2022-06-30 15:13:09 -0500

    Update titanic.R file

--- src/titanic.R
+++ src/titanic.R
@@ -50,12 +50,15 @@ createTable(
   compareGroups(Survived ~ Embarked, data = d.train, method = 3, subset = Embarked != ""),
 show.ratio = TRUE)
 
+# histograms for data distribution
+hist(d.train$Fare, breaks = 20)
+
 
 ###############################################################################################
 ### logistic regression ###
 
 # data set to use for logistic regression
-d.logistic <- select(d.train, Survived, Pclass, Sex, SibSp, Parch, Fare) %>%
+d.logistic <- select(d.train, Survived, Pclass, Age, Sex, SibSp, Parch, Fare) %>%
 
   # remove any rows with missing values
   na.omit()
@@ -70,15 +73,18 @@ step(logistic.1, scope = Survived ~ 1, direction = "both", trace = FALSE)
 
 # chosen logistic regression model
 summary(
-  logistic.2 <- glm(formula = Survived ~ Pclass + Sex + SibSp + Parch + Fare +
-    Pclass:Sex + Pclass:SibSp + Pclass:Parch + Pclass:Fare +
-    Sex:Parch + Parch:Fare, family = "binomial", data = d.logistic)
+  logistic.2 <- glm(formula = Survived ~ Pclass + Age + Sex + SibSp + Parch +
+    Fare + Pclass:Sex + Pclass:SibSp + Pclass:Parch + Age:Sex +
+    Age:Parch + Age:Fare, family = "binomial", data = d.logistic)
 )
 
 # test if any elements should be removed
 drop1(logistic.2)
 
-# survival prediction for test data
-pred.log2 <- predict(logistic.2, newdata = d.test, type = "response")
+# survival prediction for train data
+pred.train.logistic2 <- if_else(predict(logistic.2, newdata = d.logistic, type = "response") > 0.5, 1, 0)
 
-d.test$Surv_pred <- if_else(pred.log2 > 0.5, 1, 0)
+# compare actual and predicted survival for train data
+table(pred.train.logistic2, d.logistic$Survived)
+
+sum(pred.train.logistic2 == d.logistic$Survived) / nrow(d.logistic) * 100
